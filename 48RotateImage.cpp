af81d7687409e7069d960271c3e02f75dc878b40
hamidrezamousavi <hamidreza.mousavi@yahoo.com>
2023-05-11 14:38:20 +0330

    correct error

--- 48RotateImage.cpp
+++ 48RotateImage.cpp
@@ -1,33 +1,22 @@
- void rotateNth(vector<vector<int>>& matrix, int n){
-  int rowBegin = n;
-  int rowEnd = matrix.size()- n-1;
-  int colBegin = n;
-  int colEnd = matrix[0].size()-n-1;
-  int ms = matrix.size();
-for(int j{0};j< ms-n-1;j++){
-  int temp = matrix[rowBegin ][colBegin];
-for(int i{0}; i < ms-2*n-1; i++){
-  matrix[rowBegin + i][colBegin] = matrix[rowBegin+i+1][colBegin]; 
-}
-for(int i{0};i < ms-2*n-1; i++){
-  matrix[rowEnd][colBegin+i] = matrix[rowEnd][colBegin+i+1]; 
-  
-}
-for(int i{0};i < ms-2*n-1; i++){
-  matrix[rowEnd-i][colEnd] = matrix[rowEnd-i-1][colEnd]; 
-  
-}
-for(int i{0};i < ms-2*n-1; i++){
-  matrix[rowBegin][colEnd-i] = matrix[rowBegin][colEnd-i-1]; 
-  
-}
-
-matrix[rowBegin][colBegin+1] = temp;
-}
-}
-
-void rotate(vector<vector<int>>& matrix) {
-  int rotateN = matrix.size()/2;
-  for(int n{0}; n< rotateN;n++)
-    rotateNth(matrix, n);
-}
+ class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int g=0;g<matrix.size();g++){
+            for(int i=0,j=g;j<matrix.size();i++,j++){
+                int t=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=t;
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            int l=0,h=matrix.size()-1;
+            while(l<h){
+                int t=matrix[i][l];
+                matrix[i][l]=matrix[i][h];
+                matrix[i][h]=t;
+                l++;
+                h--;
+            }
+        }
+    }
+};
